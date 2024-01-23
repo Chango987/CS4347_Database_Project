6e2257e479677e5723dba9918236a75febe5bc6a
jeremu2907 <jeremu2907@gmail.com>
2024-01-23 14:49:33 -0600

    Set up token for backend access

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,6 +120,14 @@ DATABASES = {
 #         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
 #     },
 # ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+ACCESS_TOKEN_LIFETIME = timedelta(minutes=5)
 
 AUTH_USER_MODEL = "users.User"
 
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.hashers import make_password, check_password
 
-# Create your views here.
+from .models import User
+
+@APIView('GET')
+def user_token(request):
+    if request.method != 'GET':
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    data = request.GET
+    try:
+        user = User.objects.get(email=data['email'])
+        if not check_password(data['password'], user.password):
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
+        token = RefreshToken.for_user(user)
+        user['token'] = token
+        return Response(user)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
