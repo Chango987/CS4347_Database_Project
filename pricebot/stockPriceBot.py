de18c2b46df0b04b2251a1ad412a9dad52440134
jeremu2907 <jeremu2907@gmail.com>
2024-01-28 10:15:59 -0600

    logging

--- pricebot/stockPriceBot.py
+++ pricebot/stockPriceBot.py
@@ -2,9 +2,11 @@ import psycopg2
 import os
 import dotenv
 import sched, time
+import logging
 
 dotenv.load_dotenv()
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 try:
     conn = psycopg2.connect(
         host=os.getenv("DATABASE_HOST"),
@@ -16,27 +18,27 @@ try:
 
     cur = conn.cursor()
 
-    print('PostgreSQL database version:')
+    logging.info('PostgreSQL database version:')
     cur.execute('SELECT version()')
 
     # display the PostgreSQL database server version
     db_version = cur.fetchone()
-    print(db_version)
-    print("CONNECTION TO DB ESTABLISHED")
+    logging.info(db_version)
+    logging.info("CONNECTION TO DB ESTABLISHED")
     cur.close()
 
 except (Exception, psycopg2.DatabaseError) as error:
-    print(error)
+    logging.error(error)
     if conn is not None:
         conn.close()
-        print('Database connection closed.')
+        logging.info('Database connection closed.')
     
     exit()
 
 def bot(scheduler):
     scheduler.enter(60, 1, bot, (scheduler,))
     try:
-        print("NEXT UPDATE ITERATION")
+        logging.info("NEXT UPDATE ITERATION")
         # Open connection
         cur = conn.cursor()
         
@@ -45,11 +47,11 @@ def bot(scheduler):
         # Close connection
         cur.close()
     except(Exception, psycopg2.DatabaseError) as error:
-        print("STOCK PRICE UPDATE FAILURE")
-        print(error)
+        logging.error("STOCK PRICE UPDATE FAILURE")
+        logging.error(error)
         if conn is not None:
             conn.close()
-            print('Database connection closed.')
+            logging.info('Database connection closed.')
         
         exit()
 
