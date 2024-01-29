4936cadf4c6f81e47fc3e1c8409b2f0da960b07c
Pouria2343 <pxm220043@utdallas.edu>
2024-01-29 12:13:13 -0600

    Piechart component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 // App.js
 import './App.css';
 import './index.css';
+
 import Signup from './components/Signup'; //  Signup component
 import LoginForm from './components/LoginForm';
+import Piechart from './components/Piechart';
 
 function App() {
   return (
     <div className="App">
       <Signup />
       <LoginForm />
+      <Piechart />
     </div>
   );
 }
