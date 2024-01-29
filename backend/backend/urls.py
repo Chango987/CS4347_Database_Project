cdb507f83ad5a6edfcbe456fc44fa77c149c4e9f
jeremu2907 <jeremu2907@gmail.com>
2024-01-29 14:09:41 -0600

    fixed syntax

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('stocks/', ViewStocks.as_view(), name='stocks'),
     path('user_stocks/', ViewUserStocks.as_view(), name='user_stocks'),
     path('user_portfolio/', ViewUserPortfolio.as_view(), name='user_portfolio'),
-    path('user_portfolio_actual/', ViewUserPortfolioActual.as_view(), name='user_portfolio_actual')
+    path('user_portfolio_actual/', ViewUserPortfolioActual.as_view(), name='user_portfolio_actual'),
     path('stocks_suggestions/', ViewStocksSuggestions.as_view(), name='stocks_suggestions')
 ]
