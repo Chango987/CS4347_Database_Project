d8a8935f7a5b21330708fb156affd1ce9d33e0ad
jeremu2907 <jeremu2907@gmail.com>
2024-01-30 14:50:42 -0600

    fixed lint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 import Home from './pages/Home';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
