c79a0cbccff11e78cdbd48ee77bc9f9fb5acc520
Pouria2343 <pxm220043@utdallas.edu>
2024-01-30 16:43:12 -0600

    stuff

--- frontend/src/components/Piechart.jsx
+++ frontend/src/components/Piechart.jsx
@@ -1,7 +1,7 @@
 import { PieChart, Pie, Cell } from 'recharts';
 
 const data = [
-  { name: 'Group A', value: 1000 },
+  { name: 'Group A', value: 400 },
   { name: 'Group B', value: 300 },
   { name: 'Group C', value: 300 },
   { name: 'Group D', value: 200 },
