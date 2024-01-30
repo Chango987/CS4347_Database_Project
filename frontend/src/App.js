b65692692e05ef44f082bcd9031f97538233fb3e
jeremu2907 <jeremu2907@gmail.com>
2024-01-30 14:41:58 -0600

    fixed structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
+import Home from './pages/Home';
+
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route />
+                <Route path="/" element={<Home />}/>
             </Routes>
         </BrowserRouter>
     );
