308adcd7fb8d42735a8b68fc79964bf305253305
tracyhuynh <thh220000@utdallas.edu>
2024-02-03 16:39:01 -0600

    finished get and put endpt for UserCashBalance

--- backend/users/views.py
+++ backend/users/views.py
@@ -10,7 +10,7 @@ from rest_framework import status
 from django.contrib.auth.hashers import make_password, check_password
 from django.db import connection
 
-from .models import User, UserPortfolio
+from .models import User, UserPortfolio, UserCashBalance
 
 class UsersSerializer(serializers.ModelSerializer):
     class Meta:
@@ -164,6 +164,64 @@ class ViewUserPortfolio(APIView):
         return Response(status=status.HTTP_200_OK)
     
 
+class UserCashBalanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserCashBalance
+        fields = [
+            "current_cash_balance",
+            "proj_1year_balance",
+            "proj_5year_balance"
+        ] 
+
+
 class ViewUserCashBalance(APIView):
+    permission_classes = [IsAuthenticated]
     def get(self, request, format=None):
-        return Response(status=status.HTTP_200_OK)
+        user = request.user
+        cash_balance = UserCashBalance.objects.raw("SELECT * FROM users_usercashbalance WHERE user_id = %s", [user.id])[0]
+        cash_balance = UserCashBalanceSerializer(cash_balance).data
+
+        return Response(cash_balance)
+    
+    def put(self, request, format=None):
+        user = request.user
+        data = request.data
+
+        current_cash_balance = data["current_cash_balance"]
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT COUNT(*) FROM users_usercashbalance WHERE user_id = %s",
+                [user.id]
+            )
+            result = cursor.fetchone()[0]
+
+            if result > 0:
+                sql_statement = f"""
+                    UPDATE users_usercashbalance
+                    SET current_cash_balance = %s
+                    WHERE user_id = %s
+                """
+                cursor.execute(sql_statement,[current_cash_balance, user.id])
+                connection.commit()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+
+            else:
+                sql_statement = f"""
+                    INSERT into users_usercashbalance (
+                        user_id,
+                        current_cash_balance,
+                        proj_1year_balance,
+                        proj_5year_balance
+                    )
+                    values
+                        (%s, %s, %s, %s)
+                """
+                cursor.execute(sql_statement,[
+                    user.id,
+                    current_cash_balance,
+                    current_cash_balance,
+                    current_cash_balance
+                ])
+                connection.commit()
+                return Response(status=status.HTTP_201_CREATED)
