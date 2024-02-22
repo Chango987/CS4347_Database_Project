87752d6ea98f6a8f0bcbfaa031b0fd4a2f9bb5f4
Pouria2343 <pxm220043@utdallas.edu>
2024-02-22 14:51:12 -0600

    chart pulling data from db

--- frontend/src/components/Piechart.jsx
+++ frontend/src/components/Piechart.jsx
@@ -1,3 +1,5 @@
+
+
 import { PieChart, Pie, Cell } from 'recharts';
 import StockStatsTable from './StockStatsTable';
 import { backendURL, getAuthHeader } from '../utils';
@@ -48,19 +50,49 @@ const renderCustomizedLabel = ({
         ></text>
         </>
     );
-    };
+};
 
 const Piechart = () => {
     // eslint-disable-next-line
     const [apiData, setApiData] = useState([]);
-    const fetchData = async () => {
-            try {
-                const response = await axios.get(`${backendURL}/user_stocks/`, getAuthHeader());
-                setApiData(response.data);
-            } catch (error) {
-                console.error('Error fetching data:', error);
+    const [spread, setSpread] = useState(null);
+
+    const calcSpread = (stockList) => {
+        let sStocks = 0;
+        let mStocks = 0;
+        let lStocks = 0;
+        for (const stock of stockList) {
+            if (stock.stocks.cap_size === 's') {
+                sStocks += 1;
             }
-        };
+            else if (stock.stocks.cap_size === 'm') {
+                mStocks += 1;
+            }
+            else {
+                lStocks += 1;
+            }
+        }
+
+        sStocks = (sStocks / stockList.length) * 100;
+        mStocks = (mStocks / stockList.length) * 100;
+        lStocks = (lStocks / stockList.length) * 100;
+
+        setSpread([
+            { name: 'small', value: parseFloat(sStocks.toFixed(2)) },
+            { name: 'medium', value: parseFloat(mStocks.toFixed(2)) },
+            { name: 'large', value: parseFloat(lStocks.toFixed(2)) },
+        ]);
+    };
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(`${backendURL}/user_stocks/`, getAuthHeader());
+            setApiData(response.data);
+            calcSpread(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
 
     useEffect(() => {
         fetchData();
@@ -122,14 +154,14 @@ const Piechart = () => {
 
                 <PieChart width={400} height={400}>
                     <Pie
-                    data={data}
-                    cx="50%"
-                    cy="50%"
-                    labelLine={false}
-                    label={renderCustomizedLabel}
-                    outerRadius={150}
-                    fill="#8884d8"
-                    dataKey="value"
+                        data={spread}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={150}
+                        fill="#8884d8"
+                        dataKey="value"
                     >
                     {data.map((entry, index) => (
                         <Cell
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -9,6 +9,7 @@ function Dashboard() {
         <br />
 
             <Dropdown/>
+            
 
             <Dropdown/>
 
